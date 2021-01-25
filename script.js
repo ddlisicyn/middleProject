e3edcd8f35566488eadfefc27cfa73fae5618bd8
ddlisicyn <ddlisicyn@mail.ru>
2021-01-25 22:54:23 +0500

    2nd commit addaptive

---
+++ script.js
@@ -0,0 +1,9 @@
+/* let images = document.querySelectorAll(".slider img");
+
+function slider() {
+    images.forEach(item => {
+    item.classList.toggle('innactive');
+    }, 3000);
+}
+
+slider(); */
